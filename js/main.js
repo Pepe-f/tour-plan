dddc58fcb20fbb92ad013545d7b0f2c3263bd4ea
Pepe-f <nik.kopylov.8080@mail.ru>
2022-03-01 01:20:49 +0300

    create: swiper-slider in hotel section

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button--next',
+    prevEl: '.slider-button--prev',
+  },
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+});
