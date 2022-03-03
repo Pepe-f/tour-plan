9a8161a717609020d66e079250f9b0cf92a98e01
Pepe-f <nik.kopylov.8080@mail.ru>
2022-03-04 00:07:19 +0300

    create: reviews section and fixed slider

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-const swiper = new Swiper('.swiper', {
+const hotelSlider = new Swiper(".hotel-slider", {
   // Optional parameters
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: '.slider-button--next',
-    prevEl: '.slider-button--prev',
+    nextEl: ".hotel-slider__button--next",
+    prevEl: ".hotel-slider__button--prev",
   },
 
   keyboard: {
@@ -14,4 +14,20 @@ const swiper = new Swiper('.swiper', {
   },
 });
 
-$('.newsletter').parallax({imageSrc: '../img/newsletter-bg.jpg', speed: 0.7});
+$(".newsletter").parallax({ imageSrc: "../img/newsletter-bg.jpg", speed: 0.7 });
+
+const reviewsSlider = new Swiper(".reviews-slider", {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".reviews-slider__button--next",
+    prevEl: ".reviews-slider__button--prev",
+  },
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+});
